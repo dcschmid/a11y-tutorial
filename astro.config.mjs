df973e6bcf5b5a56d977010d8b4a6fa1c9928668
Daniel Schmid <daniel.schmid@mailbox.org>
2023-04-02 19:23:24 +0200

    clean up and create new real acrticles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
+import remarkToc from 'remark-toc'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()]
-});
+  integrations: [preact(), mdx()],
+  markdown: {
+    // Applied to .md and .mdx files
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeHeadingIds],
+  },
+})
