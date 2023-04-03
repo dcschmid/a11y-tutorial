764ce8755748f0d6ec3a82dcce37540c56e3621b
Daniel Schmid <daniel.schmid@mailbox.org>
2023-04-03 22:24:30 +0200

    add mdx code block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
-
+import AutoImport from 'astro-auto-import'
 import mdx from '@astrojs/mdx'
-import remarkToc from 'remark-toc'
-import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), mdx()],
-  markdown: {
-    // Applied to .md and .mdx files
-    remarkPlugins: [remarkToc],
-    rehypePlugins: [rehypeHeadingIds],
-  },
+  integrations: [
+    AutoImport({
+      imports: [mdxCodeBlockAutoImport('./src/components/CodeBlock.astro')],
+    }),
+    MDXCodeBlocks(),
+    preact(),
+    mdx(),
+  ],
 })
